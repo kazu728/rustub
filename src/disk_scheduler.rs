4dc9b75c5bdab784e237676c71334718e7a112bd
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2024-04-26 22:21:07 +0900

    Refactor DiskScheduler to use Arc<Mutex<[u8; PAGE_SIZE]>> for Read and Write requests

--- src/disk_scheduler.rs
+++ src/disk_scheduler.rs
@@ -1,13 +1,16 @@
 use crate::disk_manager::DiskManagerError;
+use crate::PAGE_SIZE;
 use crate::{disk_manager::DiskManager, PageId};
 use std::collections::VecDeque;
+use std::io;
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 
 #[derive(Debug)]
 pub enum Request {
-    Read(PageId, Vec<u8>),
-    Write(PageId, Vec<u8>),
+    Read(PageId, Arc<Mutex<[u8; PAGE_SIZE]>>),
+    Write(PageId, Arc<Mutex<[u8; PAGE_SIZE]>>),
+    Shutdown,
 }
 
 #[derive(Debug)]
@@ -17,19 +20,18 @@ pub enum DiskSchedulerError {
 
 #[derive(Debug)]
 pub struct DiskScheduler {
-    request_queue: Arc<Mutex<VecDeque<Request>>>,
-    worker: Option<JoinHandle<()>>,
+    pub request_queue: Arc<Mutex<VecDeque<Request>>>,
+    pub worker: io::Result<JoinHandle<()>>,
 }
 
 impl DiskScheduler {
     pub fn new(file_name: &str) -> Result<Self, DiskSchedulerError> {
         let request_queue = Arc::new(Mutex::new(VecDeque::new()));
-
         let disk_manager = Arc::new(Mutex::new(
             DiskManager::new(file_name).map_err(DiskSchedulerError::DiskManagerError)?,
         ));
 
-        let worker = Some(Self::spawn_worker(disk_manager, request_queue.clone()));
+        let worker = Self::spawn_worker(disk_manager, request_queue.clone());
 
         Ok(Self {
             request_queue,
@@ -37,31 +39,88 @@ impl DiskScheduler {
         })
     }
 
-    fn spawn_worker(
-        disk_manager: Arc<Mutex<DiskManager>>,
+    pub fn spawn_worker(
+        mutex_disk_manager: Arc<Mutex<DiskManager>>,
         request_queue: Arc<Mutex<VecDeque<Request>>>,
-    ) -> JoinHandle<()> {
-        // TODO: thread のエラーハンドリング
-        std::thread::spawn(move || loop {
-            let request = request_queue.lock().unwrap().pop_front();
+    ) -> Result<JoinHandle<()>, io::Error> {
+        let handle = std::thread::Builder::new()
+            .spawn(move || loop {
+                let request = request_queue.lock().unwrap().pop_front();
+                let mut disk_manager = mutex_disk_manager.lock().unwrap();
 
-            if let Some(request) = request {
-                match request {
-                    Request::Read(page_id, mut page_data) => disk_manager
-                        .lock()
-                        .unwrap()
-                        .read(page_id, &mut page_data)
-                        .unwrap(),
+                if let Some(request) = request {
+                    match request {
+                        Request::Read(page_id, page_data) => {
+                            disk_manager
+                                .read(page_id, &mut *page_data.lock().unwrap())
+                                .unwrap();
+                        }
 
-                    Request::Write(page_id, mut page_data) => disk_manager
-                        .lock()
-                        .unwrap()
-                        .write(page_id, &mut page_data)
-                        .unwrap(),
+                        Request::Write(page_id, page_data) => disk_manager
+                            .write(page_id, &mut *page_data.lock().unwrap())
+                            .unwrap(),
+                        Request::Shutdown => break,
+                    }
+                } else {
+                    thread::sleep(std::time::Duration::from_millis(100));
                 }
-            } else {
-                thread::sleep(std::time::Duration::from_millis(100));
-            }
-        })
+            })
+            .map_err(|err| io::Error::new(io::ErrorKind::Other, err.to_string()))?;
+        Ok(handle)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_job() {
+        let disk_scheduler = DiskScheduler::new("test.db").unwrap();
+
+        let buf = Arc::new(Mutex::new([0; PAGE_SIZE]));
+
+        disk_scheduler
+            .request_queue
+            .lock()
+            .unwrap()
+            .push_back(Request::Read(0, Arc::clone(&buf)));
+
+        disk_scheduler
+            .request_queue
+            .lock()
+            .unwrap()
+            .push_back(Request::Shutdown);
+
+        disk_scheduler.worker.unwrap().join().unwrap();
+
+        let buf_locked = buf.lock().unwrap();
+
+        assert_eq!(*buf_locked, [0; PAGE_SIZE]);
+    }
+
+    #[test]
+    fn write_job() {
+        let disk_scheduler = DiskScheduler::new("test.db").unwrap();
+
+        let buf = Arc::new(Mutex::new([1; PAGE_SIZE]));
+
+        disk_scheduler
+            .request_queue
+            .lock()
+            .unwrap()
+            .push_back(Request::Write(1, Arc::clone(&buf)));
+
+        disk_scheduler
+            .request_queue
+            .lock()
+            .unwrap()
+            .push_back(Request::Shutdown);
+
+        disk_scheduler.worker.unwrap().join().unwrap();
+
+        let buf_locked = buf.lock().unwrap();
+
+        assert_ne!(*buf_locked, [0; PAGE_SIZE]);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 use std::sync::atomic::AtomicUsize;
 
+use chrono::Utc;
+
+pub mod buffer_pool_manager;
 pub mod disk_manager;
 pub mod disk_scheduler;
 pub mod replacer;
@@ -30,8 +33,22 @@ impl Page {
         }
     }
 
+    pub fn is_unused(&self) -> bool {
+        self.pin_count == 0
+    }
+
     pub fn get_id(&self) -> PageId {
         self.id
+    }
+
+    pub fn pin(&mut self) {
+        self.pin_count += 1;
+        // TODO: 最終更新日時はReplacerの関心なのでReplacerに移した方がいいかもしれない(replace戦略に依存する)
+        self.last_used_at = Utc::now().timestamp_millis();
+    }
+
+    pub fn unpin(&mut self) {
+        self.pin_count -= 1;
     }
 }
 type Frames = HashMap<FrameId, Page>;
