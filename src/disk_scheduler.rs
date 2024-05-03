8493be4910b27b6964d7ebd27269abfeeefdb52f
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2024-05-04 03:38:29 +0900

    refactor disk_scheduler

--- src/disk_scheduler.rs
+++ src/disk_scheduler.rs
@@ -14,35 +14,44 @@ pub enum Request {
 #[derive(Debug)]
 pub enum DiskSchedulerError {
     DiskManagerError(DiskManagerError),
+    SendError,
+    RecvError(mpsc::RecvError),
+    UnexpectedError,
 }
 
 #[derive(Debug)]
 pub struct DiskScheduler {
     pub request_queue: Arc<Mutex<VecDeque<Request>>>,
-    pub worker: io::Result<JoinHandle<()>>,
+    pub workers: Vec<io::Result<JoinHandle<Result<(), DiskSchedulerError>>>>,
 }
 
 impl DiskScheduler {
-    pub fn new(file_name: &str) -> Result<Self, DiskSchedulerError> {
+    pub fn new(file_name: &str) -> Result<DiskScheduler, DiskSchedulerError> {
         let request_queue = Arc::new(Mutex::new(VecDeque::new()));
         let disk_manager = Arc::new(Mutex::new(
             DiskManager::new(file_name).map_err(DiskSchedulerError::DiskManagerError)?,
         ));
 
-        let worker = Self::spawn_worker(disk_manager, request_queue.clone());
+        let mut workers = Vec::with_capacity(4);
+
+        for _ in 0..4 {
+            let worker =
+                DiskScheduler::spawn_worker(Arc::clone(&disk_manager), Arc::clone(&request_queue));
+            workers.push(worker);
+        }
 
         Ok(Self {
             request_queue,
-            worker,
+            workers,
         })
     }
 
     // TODO: ロックの範囲を狭める
     // ディスクの書き込みが遅いのである程度バッファに溜めてから書き込むようにする
-    pub fn spawn_worker(
+    fn spawn_worker(
         mutex_disk_manager: Arc<Mutex<DiskManager>>,
         request_queue: Arc<Mutex<VecDeque<Request>>>,
-    ) -> Result<JoinHandle<()>, io::Error> {
+    ) -> Result<JoinHandle<Result<(), DiskSchedulerError>>, io::Error> {
         let handle = std::thread::Builder::new()
             .spawn(move || loop {
                 let request = request_queue.lock().unwrap().pop_front();
@@ -53,15 +62,19 @@ impl DiskScheduler {
                         Request::Read(page_id, page_data, sender) => {
                             disk_manager
                                 .read(page_id, &mut *page_data.lock().unwrap())
-                                .unwrap();
-                            sender.send(()).unwrap();
+                                .map_err(|err| DiskSchedulerError::DiskManagerError(err))
+                                .and_then(|_| {
+                                    sender.send(()).map_err(|_| DiskSchedulerError::SendError)
+                                })?;
                         }
 
                         Request::Write(page_id, page_data, sender) => {
                             disk_manager
                                 .write(page_id, &mut *page_data.lock().unwrap())
-                                .unwrap();
-                            sender.send(()).unwrap();
+                                .map_err(|err| DiskSchedulerError::DiskManagerError(err))
+                                .and_then(|_| {
+                                    sender.send(()).map_err(|_| DiskSchedulerError::SendError)
+                                })?;
                         }
                     }
                 } else {
@@ -70,6 +83,45 @@ impl DiskScheduler {
             })
             .map_err(|err| io::Error::new(io::ErrorKind::Other, err.to_string()))?;
         Ok(handle)
+    }
+
+    pub fn read(
+        &self,
+        page_id: PageId,
+        page_data: Arc<Mutex<[u8; PAGE_SIZE]>>,
+    ) -> Result<(), DiskSchedulerError> {
+        let (sender, receiver) = mpsc::channel();
+
+        self.request_queue.lock().unwrap().push_back(Request::Read(
+            page_id,
+            Arc::clone(&page_data),
+            sender,
+        ));
+
+        receiver
+            .recv()
+            .map_err(|err| DiskSchedulerError::RecvError(err))?;
+
+        Ok(())
+    }
+
+    pub fn write(
+        &self,
+        page_id: PageId,
+        page_data: Arc<Mutex<[u8; PAGE_SIZE]>>,
+    ) -> Result<(), DiskSchedulerError> {
+        let (sender, receiver) = mpsc::channel();
+
+        self.request_queue
+            .lock()
+            .unwrap()
+            .push_back(Request::Write(page_id, page_data, sender));
+
+        receiver
+            .recv()
+            .map_err(|err| DiskSchedulerError::RecvError(err))?;
+
+        Ok(())
     }
 }
 
@@ -83,17 +135,9 @@ mod tests {
         let file_name = "disk_scheduler_read_job.db";
         let disk_scheduler = DiskScheduler::new(file_name).unwrap();
 
-        let (sender, receiver) = mpsc::channel();
-
         let buffer = Arc::new(Mutex::new(fixture::create_random_binary_page_data()));
 
-        disk_scheduler
-            .request_queue
-            .lock()
-            .unwrap()
-            .push_back(Request::Read(0, Arc::clone(&buffer), sender));
-
-        let result = receiver.recv();
+        let result = disk_scheduler.read(0, Arc::clone(&buffer));
 
         assert!(result.is_err());
 
@@ -105,18 +149,17 @@ mod tests {
         let file_name = "disk_scheduler_write_job.db";
         let disk_scheduler = DiskScheduler::new(&file_name).unwrap();
 
-        let (sender, receiver) = mpsc::channel();
-        let buf = Arc::new(Mutex::new(fixture::create_random_binary_page_data()));
+        let page_data = fixture::create_random_binary_page_data();
+        let buf = Arc::new(Mutex::new(page_data));
 
-        disk_scheduler
-            .request_queue
-            .lock()
-            .unwrap()
-            .push_back(Request::Write(1, Arc::clone(&buf), sender));
-
-        receiver.recv().unwrap();
+        disk_scheduler.write(1, Arc::clone(&buf)).unwrap();
 
         assert_ne!(*buf.lock().unwrap(), [0; PAGE_SIZE]);
+
+        let buffer = Arc::new(Mutex::new([0; PAGE_SIZE]));
+
+        disk_scheduler.read(1, Arc::clone(&buffer)).unwrap();
+        assert_eq!(*buffer.lock().unwrap(), page_data);
 
         tear_down(file_name)
     }
